d9680d940bc40133cc2286bfcf65c447899efae0
Eugen Iftimoaie <60259543+eugeniftimoaie@users.noreply.github.com>
2020-05-05 22:26:18 +0200

    version 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='DescribeLinearModel',
-      version='1.0.0',
+      version='1.0.1',
       description='class for calculating and visualizing a regression line',
       author='Eugen Iftimoaie',
       packages=['DescribeLinearModel'],
